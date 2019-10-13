5782643d334a898bb89b38758f20eb6527044c7f
mohammad alkhalil <38015934+mskhaleel@users.noreply.github.com>
2019-10-13 10:56:31 +0300

    add event listener

--- view.php
+++ view.php
@@ -60,6 +60,10 @@ $PAGE->set_heading(format_string($course->fullname));
 $completion = new completion_info($course);
 $completion->set_module_viewed($cm);
 
+// Let's add the module viewed event.
+$event = \mod_collaborate\event\page_viewed::create(['context' => $PAGE->context]);
+$event->trigger();
+
 // The renderer performs output to the page.
 $renderer = $PAGE->get_renderer('mod_collaborate');
 
